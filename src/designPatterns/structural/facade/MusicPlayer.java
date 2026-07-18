d38cf9aa8f40770c4eab9b10921ad2e63541ff31
Maheboob Kankurti <maheboobkankurti@gmail.com>
2026-07-19 01:18:17 +0530

    Facade design pattern example

---
+++ src/designPatterns/structural/facade/MusicPlayer.java
@@ -0,0 +1,14 @@
+package designPatterns.structural.facade;
+
+public class MusicPlayer {
+
+    public void initializeAudioDrivers(){
+        System.out.println("Audio drivers initialized.");
+    }
+    public void decodeAudio() {
+        System.out.println("Audio decoded.");
+    }
+    public void startPlayback() {
+        System.out.println("Music playback started.");
+    }
+}
---
+++ src/designPatterns/structural/facade/VideoPlayer.java
@@ -0,0 +1,16 @@
+package designPatterns.structural.facade;
+
+public class VideoPlayer {
+
+    public void setupRenderingEngine(){
+        System.out.println("Rendering engine set up.");
+    }
+
+    public void loadVideoFile(){
+        System.out.println("Video file loaded.");
+    }
+
+    public void playVideo(){
+        System.out.println("Video playback started.");
+    }
+}
